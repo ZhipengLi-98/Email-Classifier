181335aa58774f3b3c559d586b1eef323234e03c
arsenalahm <clevercake7@gmail.com>
2019-03-25 22:52:12 +0800

    Use k-fold cross validation to train and test

--- naivebayes.py
+++ naivebayes.py
@@ -9,19 +9,25 @@ class NaiveBayes:
     trainSize = 1
     features = {'ham': {}, 'spam': {}}
     probs = {'ham': 0, 'spam': 0}
-    totalNum = 0
 
-    def calcPro(self):
+    def calcPro(self, fold):
+        print('Calc Fold ' + str(fold) + ' Start')
         # calculate probility -- y
-        trainData = dataparser.readSets('trainSet.data')
+        trainData = []
+        for i in range(dataparser.folds):
+            if not i == fold:
+                temp = dataparser.readSets('trainSet' + str(i) + '.data')
+                for j in temp:
+                    trainData.append(j)
+        # trainData = dataparser.readSets('trainSet.data')
         cnt = 0
         for data in trainData:
             cnt += 1
             if random.random() > self.trainSize:
                 continue
 
-            if cnt % 1000 == 0:
-                print(cnt)
+            if cnt % 500 == 0:
+                print(float(cnt) / float(len(trainData)) * 100)
             emailPath = data[0]
             label = data[1]
             # emailPath: '../data/xxx/xxx'
@@ -47,29 +53,37 @@ class NaiveBayes:
             self.probs['spam'] += i
         for i in self.features['ham'].values():
             self.probs['ham'] += i
-        file = open(dataparser.dirPath + '/probs.data', 'w', encoding='utf-8')
+        file = open(dataparser.dirPath + '/probs' + str(fold) + '.data', 'w', encoding='utf-8')
         json.dump(self.probs, file, indent=4)
         print("Probs Done")
 
-        file = open(dataparser.dirPath + '/features.data', 'w', encoding='utf-8')
+        file = open(dataparser.dirPath + '/features' + str(fold) + '.data', 'w', encoding='utf-8')
         json.dump(self.features, file, ensure_ascii=False, indent=4)
         print("Features Done")
 
-    def classify(self):
+        print('Calc Fold ' + str(i) + ' Done')
+
+    def classify(self, fold):
+        print('Classify Fold ' + str(fold) + ' Start')
         # classify emails
         correctCnt = 0
         totalCnt = 0
-        testSet = dataparser.readSets('testSet.data')
-        probsFile = open(dataparser.dirPath + '/probs.data')
+        spamCnt = 0
+        spamCorCnt = 0
+        recallCnt = 0
+        recallCorCnt = 0
+        totalNum = 0
+        testSet = dataparser.readSets('trainSet' + str(fold) + '.data')
+        probsFile = open(dataparser.dirPath + '/probs' + str(fold) + '.data')
         probs = json.load(probsFile)
-        featuresFile = open(dataparser.dirPath + '/features.data', encoding='utf-8')
+        featuresFile = open(dataparser.dirPath + '/features' + str(fold) + '.data', encoding='utf-8')
         features = json.load(featuresFile)
         for num in probs.values():
-            self.totalNum += num
+            totalNum += num
         for data in testSet:
             result = ''
-            hamProb = float(probs['ham']) / float(self.totalNum)
-            spamProb = float(probs['spam']) / float(self.totalNum)
+            hamProb = float(probs['ham']) / float(totalNum)
+            spamProb = float(probs['spam']) / float(totalNum)
             emailPath = data[0]
             label = data[1]
             email = open(dataparser.dirPath + '/data_cut/' + emailPath[8:], encoding='utf-8')
@@ -89,20 +103,50 @@ class NaiveBayes:
 
             if spamProb > hamProb:
                 result = 'spam'
+                spamCnt += 1
             else:
                 result = 'ham'
             totalCnt += 1
+            if label == 'spam':
+                recallCnt += 1
             if result == label:
                 correctCnt += 1
-            # print(float(correctCnt) / float(totalCnt))
+                if label == 'spam':
+                    spamCorCnt += 1
+                    recallCorCnt += 1
+            print(float(totalCnt) / float(len(testSet)) * 100)
 
         accuracy = float(correctCnt) / float(totalCnt)
-        print('Correct: ', correctCnt)
-        print('Total: ', totalCnt)
-        print('Accuracy: ', accuracy)
+        precision = float(spamCorCnt) / float(spamCnt)
+        recall = float(recallCorCnt) / float(recallCnt)
+        f1_measure = float(2) * float(precision) * float(recall) / (float(precision) + float(recall))
+        # print('Test ID', fold)
+
+        print('Classify Fold ' + str(fold) + ' Done')
+        return [accuracy, precision, recall, f1_measure]
+
+    def crossClassify(self):
+        results = []
+        for i in range(dataparser.folds):
+            # trainSeti.data as vertification set
+            self.calcPro(i)
+            results.append(self.classify(i))
+
+        accuracy = 0.0
+        precision = 0.0
+        recall = 0.0
+        f1_measure = 0.0
+        for i in range(len(results)):
+            accuracy += results[i][0]
+            precision += results[i][1]
+            recall += results[i][2]
+            f1_measure += results[i][3]
+        print('Accuracy: ', float(accuracy) / dataparser.folds)
+        print('Precision', float(precision) / dataparser.folds)
+        print('Recall', float(recall) / dataparser.folds)
+        print('F1-Measure', float(f1_measure) / dataparser.folds)
 
 
 if __name__ == '__main__':
     classifier = NaiveBayes()
-    # classifier.calcPro()
-    classifier.classify()
+    classifier.crossClassify()
